05f4a4bef004138b6a3c9f701a5f5d3722e11cd6
henry.tu <henry.tu@caloudi.com>
2022-03-26 12:19:21 +0800

    added Utilize & Goverance contract files only

---
+++ scaffold-eth/packages/hardhat/contracts/UT.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.6;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract UtilizeToken is ERC20, Ownable {
+    constructor() ERC20("UtilizeToken", "UT") {
+        _mint(msg.sender, 10000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
