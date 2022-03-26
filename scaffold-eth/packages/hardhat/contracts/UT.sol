e8dd65a95bf39247834d855925dbfe5cb5c369f7
henry.tu <henry.tu@caloudi.com>
2022-03-26 17:46:02 +0800

    added UT/GT ERC-20 token

--- scaffold-eth/packages/hardhat/contracts/UT.sol
+++ scaffold-eth/packages/hardhat/contracts/UT.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract UtilizeToken is ERC20, Ownable {
     constructor() ERC20("UtilizeToken", "UT") {
-        _mint(msg.sender, 10000 * 10 ** decimals());
+        _mint(msg.sender, 0 * 10 ** decimals());
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
