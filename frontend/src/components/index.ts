5246a380784cc5229e807c9ae8e4600979077e37
Hazel <tinazx056@gmail.com>
2022-03-29 07:47:50 +0800

    fix(app.tsx): import getToken

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -2,3 +2,4 @@ export { Section1, Section2, Section3, Section4, Section5, Section6, Section7, S
 export { Header } from './Header'
 export { PostList, CreatePost } from './Post'
 export { ConnectWallet } from './ConnectWallet'
+export { GetToken } from './GetToken'
