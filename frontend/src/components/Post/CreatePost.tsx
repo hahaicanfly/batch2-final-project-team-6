1a3386359a9448ff63c41bd8edb00ceff1ced1d1
Hazel <tinazx056@gmail.com>
2022-03-29 08:26:18 +0800

    feat(post): CreatePost

--- frontend/src/components/Post/CreatePost.tsx
+++ frontend/src/components/Post/CreatePost.tsx
@@ -1,14 +1,5 @@
-import React from 'react'
 import { Form, Input, Button, Select } from 'antd';
-const { Option } = Select;
-const layout = {
-  labelCol: {
-    span: 8,
-  },
-  wrapperCol: {
-    span: 16,
-  },
-};
+
 const tailLayout = {
   wrapperCol: {
     offset: 8,
@@ -19,27 +10,6 @@ const tailLayout = {
 export const CreatePost = () => {
 
   const [form] = Form.useForm();
-
-  const onGenderChange = (value) => {
-    switch (value) {
-      case 'male':
-        form.setFieldsValue({
-          note: 'Hi, man!',
-        });
-        return;
-
-      case 'female':
-        form.setFieldsValue({
-          note: 'Hi, lady!',
-        });
-        return;
-
-      case 'other':
-        form.setFieldsValue({
-          note: 'Hi there!',
-        });
-    }
-  };
 
   const onFinish = (values) => {
     console.log(values);
@@ -57,68 +27,48 @@ export const CreatePost = () => {
   };
 
   return (
-    <div>
-      <Form {...layout} form={form} name="control-hooks" onFinish={onFinish}>
-        <Form.Item
-          name="title"
-          label="文章標題"
-          rules={[
-            {
-              required: true,
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          name="content"
-          label="文章內容"
-          rules={[
-            {
-              required: true,
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          noStyle
-          shouldUpdate={(prevValues, currentValues) => prevValues.gender !== currentValues.gender}
-        >
-          {({ getFieldValue }) =>
-            getFieldValue('gender') === 'other' ? (
-              <Form.Item
-                name="customizeGender"
-                label="Customize Gender"
-                rules={[
-                  {
-                    required: true,
-                  },
-                ]}
-              >
-                <Input />
-              </Form.Item>
-            ) : null
-          }
-        </Form.Item>
-        <Form.Item {...tailLayout}>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-          <Button htmlType="button" onClick={onReset}>
-            Reset
-          </Button>
-          <Button type="link" htmlType="button" onClick={onFill}>
-            Fill form
-          </Button>
-        </Form.Item>
-      </Form>
+    <div className="create-post">
+      <div className="container">
+        <Form form={form} name="control-hooks" onFinish={onFinish}>
+          <Form.Item
+            name="title"
+            label="文章標題"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="content"
+            label="文章內容"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item {...tailLayout}>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+            <Button htmlType="button" onClick={onReset}>
+              Reset
+            </Button>
+            <Button type="link" htmlType="button" onClick={onFill}>
+              Fill form
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
     </div>
   );
 
 }
 
 
-// ReactDOM.render(<CreatePost />, mountNode);
 export default CreatePost
