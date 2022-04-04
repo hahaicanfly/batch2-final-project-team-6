82978e424be294eed58545721287c8e317ff8f19
Hazel Wu <tinazx056@gmail.com>
2022-04-04 10:18:33 +0800

    fix(Section6): TRASHNEWS -> TRASTNEWS

--- frontend/src/components/Home/Section6.jsx
+++ frontend/src/components/Home/Section6.jsx
@@ -7,7 +7,7 @@ export const Section6 = () => {
             <div className="box">
               <div className="box-content">
                 <div className="title linear-text">贊助廣告</div>
-                <span>贊助廣告是一種付費廣告解決方案，可讓您在高流量的畫面上顯示您的資訊，藉此提升貴產業在 TRASHNEWS 中的曝光率。</span>
+                <span>贊助廣告是一種付費廣告解決方案，可讓您在高流量的畫面上顯示您的資訊，藉此提升貴產業在 TRASTNEWS 中的曝光率。</span>
                 <div className="btn-list">
                   <button className="btn btn-border">
                     瞭解更多
