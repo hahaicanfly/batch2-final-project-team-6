b0258dc6c27d0601a5e836d10f5ed6c9fc966fbf
Hazel Wu <tinazx056@gmail.com>
2022-04-04 10:15:23 +0800

    feat(Header): Header Change Dropdown

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,24 +1,132 @@
-import { useLocation } from 'react-router-dom';
 // Utils
-import { Link } from "react-router-dom";
-import { useAccount, useBalance } from 'wagmi'
+import { Menu, Dropdown, Button } from 'antd';
+import { Link, useLocation } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useProvider, useAccount, useContractRead } from 'wagmi'
 // Components
 import { ConnectWallet } from '.'
+import { gt_token_contract, ut_token_contract } from '../config/contract'
+import { handleError } from '../config/handle-error'
 // Assets
 import '../assets/style/header.scss'
 import Logo from '../assets/images/TrustNews-logos.png'
 
 export const Header = () => {
+  const provider = useProvider()
   const location = useLocation();
   // Connected account details
   const [{ data: accountData }, disconnect] = useAccount({
     fetchEns: true,
   })
 
-  // Fetching balance information
-  const [{ data: getBalance }] = useBalance({
-    addressOrName: accountData?.address,
-  });
+  const [gtBalance, setGtBalance] = useState(0)
+  const [utBalance, setUtBalance] = useState(0)
+
+  // 取得 GT Token 餘額
+  const [{ }, getGtBalanceOf] = useContractRead(
+    {
+      addressOrName: gt_token_contract.address,
+      contractInterface: gt_token_contract.abi,
+      signerOrProvider: provider,
+    },
+    'balanceOf',
+  )
+
+  // 取得 UT Token 餘額
+  const [{ }, getUtBalanceOf] = useContractRead(
+    {
+      addressOrName: ut_token_contract.address,
+      contractInterface: ut_token_contract.abi,
+      signerOrProvider: provider,
+    },
+    'balanceOf',
+  )
+
+  const menu = (
+    <Menu>
+      <Menu.Item>
+        {accountData?.address}
+      </Menu.Item>
+      <Menu.Divider />
+      <Menu.Item>
+        <div>
+          GT幣 {gtBalance} 顆
+        </div>
+      </Menu.Item>
+      <Menu.Item>
+        <div>
+          UT幣 {utBalance} 顆
+        </div>
+      </Menu.Item>
+      <Menu.Divider />
+      <Menu.Item>
+        <Link to="/posts">
+          文章列表
+        </Link>
+      </Menu.Item>
+      {
+        location.pathname === '/posts' && (
+          <Menu.Item>
+            <Link to="/post/create">
+              發表文章
+            </Link>
+          </Menu.Item>
+        )
+      }
+      <Menu.Item>
+        <div
+          onClick={disconnect}>
+          登出
+        </div>
+      </Menu.Item>
+    </Menu>
+  );
+
+  const getGtBalance = async () => {
+    try {
+      const { data, error } = await getGtBalanceOf({
+        args: [accountData?.address]
+      })
+
+      console.log('[GT]', data)
+
+      if (error) {
+        handleError(error)
+        return
+      }
+
+      const balance = parseInt(data._hex)
+      setGtBalance(balance)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const getUtBalance = async () => {
+    try {
+      const { data, error } = await getUtBalanceOf({
+        args: [accountData?.address]
+      })
+
+      if (error) {
+        handleError(error)
+        return
+      }
+
+      console.log('[UT]', data)
+
+      const balance = parseInt(data._hex)
+      setUtBalance(balance)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    getGtBalance()
+    getUtBalance()
+  }, [accountData?.address])
+
 
   return (
     <header className="nav">
@@ -34,33 +142,14 @@ export const Header = () => {
           !accountData?.address ?
             <ConnectWallet /> :
             <div>
-              {/* <button className="btn">
-                <Link to="/posts">
-                  文章列表
-                </Link>
-              </button> */}
-              <div>
+              <Dropdown
+                overlay={menu}
+                placement="bottomLeft"
+              >
                 <button className="btn btn-border">
-                  {accountData?.address.substring(0, 6)}...
+                  {accountData?.address.substring(0, 15)}...
                 </button>
-                <button
-                  className="btn btn-border"
-                  onClick={disconnect}>
-                  登出
-                </button>
-                {
-                  location.pathname === '/posts'
-                    ?
-                    <Link to="/post/create">
-                      <button className="btn btn-border">
-                        發表文章
-                      </button>
-                    </Link>
-                    :
-                    <></>
-                }
-
-              </div>
+              </Dropdown>
             </div>
         }
       </div>
